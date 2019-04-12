38fc1f157a40d93859ea1079ddd13c9701a88525
jmvita127 <jmvita127@gmail.com>
2019-04-12 13:33:00 -0300

    Navbar modificado

--- menu.php
+++ menu.php
@@ -12,46 +12,51 @@
   </head>
   <body>
 
-    <nav class="navbar navbar-dark bg-dark">
+    <header class="header">
 
-        <a class="navbar-brand"><img src="imagenes/logo.jpg" alt="logo" witdh="100" height="110"></a>
 
-        <li class="nav-item dropdown">
-          <a class="nav-link"  href="index.php" role="link" aria-haspopup="true" aria-expanded="false">HOME</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">PC</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">COMPONENTES</a>
-            <a class="dropdown-item" href="#">PERIFERICOS</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a><img src="imagenes/logo.jpg" alt="logo" witdh="100" height="110"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php" style="color:orange;">HOME</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" style="color:orange;">COMPONENTES</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#" style="color:orange;">PC</a>
+              <a class="dropdown-item" href="#" style="color:orange;">CONSOLAS</a>
 
-            </div>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">CONSOLAS</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">SONY</a>
-            <a class="dropdown-item" href="#">NINTENDO</a>
-
-            </div>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link"  href="preguntas.php" role="button" aria-haspopup="true" aria-expanded="false">¿QUIENES SOMOS?</a>
-        </li>
-
-            <form class="form-inline">
-          <input class="form-control mr-sm-2" type="search" placeholder="Buscar Producto" aria-label="Search">
+              </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="preguntas.php" style="color:orange;">¿QUIENES SOMOS?</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
           <button class="btn" type="submit"><img src="imagenes/carrito.png" alt="" witdh="50" height="50"></button><br>
-
-      </form>
-
-        <a class="registro"  href="registro.php" role="link" >Registrarse</a>
-
-      </form>
-
-
-
+          <button type="button" class="btn btn-outline-danger">REGISTRARSE</button>
+          <button type="button" class="btn btn-outline-warning">LOG-IN</button>
+        </form>
+      </div>
     </nav>
+
+</header>
+
+
+
+
+
+
+
+
+
+
+
 
 
   </body>
